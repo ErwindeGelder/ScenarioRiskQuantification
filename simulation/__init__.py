4b21f13fc02037c3e21c96cc3b1a4ba7b3546404
Erwin de Gelder <erwin.degelder@tno.nl>
2021-06-30 11:10:10 +0200

    Simulations for approaching slower vehicle added.

--- simulation/__init__.py
+++ simulation/__init__.py
@@ -6,6 +6,8 @@ from .fastkde import KDE, kde_from_file
 from .idm import IDM, IDMParameters
 from .idmplus import IDMPlus
 from .leader_braking import LeaderBraking, LeaderBrakingParameters
+from .simulation_approaching import SimulationApproaching, idm_approaching_pars, \
+    acc_approaching_pars, acc_idm_approaching_pars
 from .simulation_lead_braking import SimulationLeadBraking, idm_lead_braking_pars, \
     acc_lead_braking_pars, acc_idm_lead_braking_pars
 from .simulation_string import SimulationString
